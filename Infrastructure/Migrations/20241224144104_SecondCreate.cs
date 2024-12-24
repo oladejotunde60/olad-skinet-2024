174d55dadbcc4ebc0a05d1f57254a5ac35a8b2e9
mubarack tahir <mubarack.tahir@fairmoney.io>
2024-12-24 17:01:35 +0100

    End of session 2

---
+++ Infrastructure/Migrations/20241224144104_SecondCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Pictureurl",
+                table: "Products",
+                newName: "PictureUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "descrioption",
+                table: "Products",
+                newName: "Description");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PictureUrl",
+                table: "Products",
+                newName: "Pictureurl");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "descrioption");
+        }
+    }
+}
